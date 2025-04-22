411a19f78e00eabbd2b7e5869b74f989cac994b2
SRfelicianoa <o0raffael@gmail.com>
2025-04-22 19:26:09 -0300

    Retirado o New

--- Trabalho_Do_Curso_2/Program.cs
+++ Trabalho_Do_Curso_2/Program.cs
@@ -18,7 +18,7 @@ namespace Trabalho_Do_Curso_2
         static ClienteServico _clienteServico;
         static void Main(string[] args) 
         {
-            _clienteServico = new ClienteServico();
+            _clienteServico = ClienteServico();
             bool executar = true;
 
             do
